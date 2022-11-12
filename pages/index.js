66c09f21a3bd1374167aa09428711345a328d81b
panosjapan7 <panosjapan@gmail.com>
2022-11-12 13:02:49 +0100

    Renders all components of landing page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,24 @@
 import Image from 'next/image'
 import Masthead from '../components/masthead'
 import Navigation from '../components/navigation'
+import PortfolioGrid from '../components/portfolio-grid'
+import Services from '../components/services'
+import About from '../components/about'
+import Team from '../components/team'
+import Clients from '../components/clients'
+import Footer from '../components/footer'
 
 export default function Home() {
   return (
     <div >
       <Navigation />
       <Masthead />
+      <Services />
+      <PortfolioGrid />
+      <About />
+      <Team />
+      <Clients />
+      <Footer />
     </div>
   )
 }
