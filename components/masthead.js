c51ff1b0097c215fb2264546f219a77e1965f4fc
panosjapan7 <panosjapan@gmail.com>
2022-11-12 16:26:50 +0100

    Add Image instead of backgrou-image from css

--- components/masthead.js
+++ components/masthead.js
@@ -1,14 +1,24 @@
-import Image from "next/image";
+import Image from 'next/legacy/image';
+import backgroundImage from "../public/img/header-bg.jpg";
 
 function Masthead () {
     return (
+
         <header className="masthead">
+            <Image 
+                src={backgroundImage}
+                alt="..."
+                layout='fill'
+                objectFit='cover'
+                objectPosition="center"
+            />
             <div className="container">
                 <div className="masthead-subheading">Welcome To Our Studio!</div>
                 <div className="masthead-heading text-uppercase">{"It's Nice To Meet You"}</div>
                 <a className="btn btn-primary btn-xl text-uppercase" href="#services">Tell Me More</a>
             </div>
         </header>
+
     )
 };
 
