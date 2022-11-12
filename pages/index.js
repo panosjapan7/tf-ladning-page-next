a9c28def568440d90fe3b5dd181f5d73ce432823
panosjapan7 <panosjapan@gmail.com>
2022-11-12 12:55:09 +0100

    Renders Mastehead component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Image from 'next/image'
+import Masthead from '../components/masthead'
 import Navigation from '../components/navigation'
 
 export default function Home() {
   return (
     <div >
       <Navigation />
+      <Masthead />
     </div>
   )
 }
