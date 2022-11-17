37292aac8ff0ba08fae62ef0f501684e5f8cc324
panosjapan7 <panosjapan@gmail.com>
2022-11-17 10:45:55 +0200

    Adds Image components

--- components/clients.js
+++ components/clients.js
@@ -1,19 +1,38 @@
+import Image from "next/legacy/image";
+import clientImageMicrosoft from "../public/img/logos/microsoft.svg";
+import clientImageGoogle from "../public/img/logos/google.svg";
+import clientImageFacebook from "../public/img/logos/facebook.svg";
+import clientImageIbm from "../public/img/logos/ibm.svg";
+
+
 function Clients () {
     return (
         <div className="py-5">
             <div className="container">
                 <div className="row align-items-center">
                     <div className="col-md-3 col-sm-6 my-3">
-                        <a href="#!"><img className="img-fluid img-brand d-block mx-auto" src="img/logos/microsoft.svg" alt="..." aria-label="Microsoft Logo" /></a>
+                        <a href="#!">
+                            <img className="img-fluid img-brand d-block mx-auto" src="img/logos/microsoft.svg" alt="..." aria-label="Microsoft Logo" />
+                            {/* <Image src={clientImageMicrosoft} alt="..." layout='responsive' className="img-fluid img-brand d-block mx-auto"/> */}
+                        </a>
                     </div>
                     <div className="col-md-3 col-sm-6 my-3">
-                        <a href="#!"><img className="img-fluid img-brand d-block mx-auto" src="img/logos/google.svg" alt="..." aria-label="Google Logo" /></a>
+                        <a href="#!">
+                            <img className="img-fluid img-brand d-block mx-auto" src="img/logos/google.svg" alt="..." aria-label="Google Logo" />
+                            {/* <Image src={clientImageGoogle} alt="..." layout='responsive' className="img-fluid img-brand d-block mx-auto"/> */}
+                        </a>
                     </div>
                     <div className="col-md-3 col-sm-6 my-3">
-                        <a href="#!"><img className="img-fluid img-brand d-block mx-auto" src="img/logos/facebook.svg" alt="..." aria-label="Facebook Logo" /></a>
+                        <a href="#!">
+                            <img className="img-fluid img-brand d-block mx-auto" src="img/logos/facebook.svg" alt="..." aria-label="Facebook Logo" />
+                            {/* <Image src={clientImageFacebook} alt="..." layout='responsive' className="img-fluid img-brand d-block mx-auto"/> */}
+                        </a>
                     </div>
                     <div className="col-md-3 col-sm-6 my-3">
-                        <a href="#!"><img className="img-fluid img-brand d-block mx-auto" src="img/logos/ibm.svg" alt="..." aria-label="IBM Logo" /></a>
+                        <a href="#!">
+                            <img className="img-fluid img-brand d-block mx-auto" src="img/logos/ibm.svg" alt="..." aria-label="IBM Logo" />
+                            {/* <Image src={clientImageIbm} alt="..." layout='responsive' className="img-fluid img-brand d-block mx-auto"/> */}
+                        </a>
                     </div>
                 </div>
             </div>
