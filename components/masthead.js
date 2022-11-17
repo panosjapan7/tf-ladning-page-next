a4114d1b0fdf624b68e61fc3144a0aca40dfd733
panosjapan7 <panosjapan@gmail.com>
2022-11-17 09:14:27 +0200

    Changes layout to responsive

--- components/masthead.js
+++ components/masthead.js
@@ -8,15 +8,17 @@ function Masthead () {
             <Image 
                 src={backgroundImage}
                 alt="..."
-                layout='fill'
+                layout='responsive'
                 objectFit='cover'
                 objectPosition="center"
             />
+
             <div className="container">
                 <div className="masthead-subheading">Welcome To Our Studio!</div>
                 <div className="masthead-heading text-uppercase">{"It's Nice To Meet You"}</div>
                 <a className="btn btn-primary btn-xl text-uppercase" href="#services">Tell Me More</a>
             </div>
+            
         </header>
 
     )
